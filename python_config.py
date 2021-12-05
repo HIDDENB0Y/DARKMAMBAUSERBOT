0ccd625d05a0770eab00e0df0a06aebe9272c38d
HIDDENBOY <94282533+HIDDENB0Y@users.noreply.github.com>
2021-12-05 20:12:59 +0530

    Update python_config.py

--- python_config.py
+++ python_config.py
@@ -161,7 +161,7 @@ if ENV:
         FORCE_SUB = os.environ.get("FORCE_SUB", None)
         FORCE_CHANNEL_UN = os.environ.get("FORCE_CHANNEL_UN", None)
         FORCE_CHANNEL_ID = int(os.environ.get("FORCE_CHANNEL_ID", False))
-        EXTRA_PYTHONBOT = os.environ.get("EXTRA_PYTHONBOT-V9.0.8", -1001221881562)
+        EXTRA_DARKMAMBA = os.environ.get("EXTRA_DARKMAMBA", -1001785131331)
         PM_DATA = os.environ.get("PM_DATA", "ENABLE")
 
 else:
