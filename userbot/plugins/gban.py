cd7628d0e4dda16d7a2cecc993901e8f2f45fbb5
HIDDENBOY <94282533+HIDDENB0Y@users.noreply.github.com>
2021-12-05 17:20:58 +0530

    Update gban.py

--- userbot/plugins/gban.py
+++ userbot/plugins/gban.py
@@ -1,6 +1,6 @@
 from userbot import bot, CMD_HELP, ALIVE_NAME
 from telethon.tl.functions.contacts import BlockRequest, UnblockRequest
-from PYTHONBOT.utils import admin_cmd, sudo_cmd, edit_or_reply
+from DARKMAMBA.utils import admin_cmd, sudo_cmd, edit_or_reply
 from userbot.cmdhelp import CmdHelp
 import html
 from telethon import events
@@ -11,7 +11,7 @@ from telethon.utils import get_input_location
 from telethon.events import ChatAction
 from . import *
 
-Legendl_Mr_Hacker = str(ALIVE_NAME) if ALIVE_NAME else "PYTHON User"
+SUKHI_MR_HACKER= str(ALIVE_NAME) if ALIVE_NAME else "DARKMAMBA User"
 papa = borg.uid
 
 
@@ -66,9 +66,10 @@ async def gban(userbot):
     if not sender.id == hum.id:
         PYTHONBOT = await edit_or_reply(ids, "Trying to gban this retard!")
     else:
-        PYTHONBOT = await edit_or_reply(ids, "`Ok! Gban ho rha h intzarrr karo....`")
+        PYTHONBOT = await edit_or_reply(ids, "`🐼  🎀  𝒜𝐵𝐸 𝒴𝒜𝒜𝑅 𝒦𝐸𝒮𝐸 𝒞𝐻𝒰𝒯𝐼𝒴𝒜 𝐼𝒩𝒮𝒜𝒜𝒩 𝒦☯ 𝒢𝐵𝒜𝒩 𝒦𝑅𝒩𝒜 𝒫𝒟𝒟 𝑅𝐻𝒜 𝐻𝒜𝐼. 𝑅𝒰𝒦𝒥𝒜𝒜 𝒴𝒜𝒜𝑅 𝒮𝒜𝐵𝒜𝑅 𝑅𝒦𝐻 𝒢𝐵𝒜𝒩 𝐻❤𝒩𝐸 𝒟𝐸  🎀  🐼
+`")
     hum = await userbot.client.get_me()
-    await PYTHONBOT.edit(f"`🔥Global Ban ho rha h ruko'💦....dekhte jaao bus kya hota h 😎 `")
+    await DARKMAMBA.edit(f"`🔥🐙  🎀  𝐿𝐸 𝐵𝐻𝒜𝐼 𝒟𝐸𝒦𝐻𝒯𝒜 𝒥𝒜𝒜 𝒦𝐼𝒯𝒩𝐸 𝑀𝐸 𝒮𝐸 𝒢𝐵𝒜𝒩 𝒥𝒜𝒴𝐸𝒢𝒜  🎀🐙😎 `")
     my_mention = "[{}](tg://user?id={})".format(hum.first_name, hum.id)
     f"@{hum.username}" if hum.username else my_mention
     await userbot.get_chat()
@@ -86,10 +87,10 @@ async def gban(userbot):
         if not reason:
             reason = "Private"
     except:
-        return await PYTHONBOT.edit(f"**Something W3NT Wrong 🤔**")
+        return await DARKMAMBA.edit(f"**🐹  🎀  𝐵𝐻𝒜𝐼 𝒦𝒰𝒞𝐻 𝒢𝒜𝒟𝐵𝒜𝒟 𝐻𝒜𝐼  🎀  🐹**")
     if user:
         if user.id == 1938996006:
-            return await PYTHONBOT.edit(
+            return await DARKMAMBA.edit(
                 f"`How dare u trying to Gban my master`"
             )
         try:
@@ -109,18 +110,18 @@ async def gban(userbot):
             try:
                 await userbot.client.edit_permissions(i, user, view_messages=False)
                 a += 1
-                await PYTHONBOT.edit(f"Gbaning ho rha h 😂\n\nTotal Chats :- `{a}`")
+                await DARKMAMBA.edit(f"𝓛𝓔 𝓑𝓔𝓣𝓐 𝓤𝓢𝓚𝓘 𝓖𝓐𝓝𝓓 𝓜𝓔 𝓖𝓑𝓐𝓝 𝓓𝓐𝓐𝓛 𝓡𝓗𝓐 𝓗𝓤 😂\n\nTotal Chats :- `{a}`")
             except:
                 b += 1
     else:
-        await PYTHONBOT.edit(f"`Either reply to a user or gib me user id/name`")
+        await DARKMAMBA.edit(f"`Either reply to a user or gib me user id/name`")
     try:
         if gmute(user.id) is False:
-            return await PYTHONBOT.edit(f"**Error! User phle se chuda(Gbanned) pda h 😂 .**")
+            return await DARKMAMBA.edit(f"**Error! User phle se chuda(Gbanned) pda h 😂 .**")
     except:
         pass
-    return await PYTHONBOT.edit(
-        f"[{user.first_name}](tg://user?id={user.id}) \n\n**Gban Successful This Person🔥\nAffected Chats😏 : {a} **"
+    return await DARKMAMBA.edit(
+        f"[{user.first_name}](tg://user?id={user.id}) \n\n**•°¯`••   🎀  𝒢𝐵𝒜𝒩 𝐻💍 𝒢𝒴𝒜 𝒴𝐸 𝐵𝐸𝒯𝒜  🎀   ••`¯°•🔥\nAffected Chats😏 : {a} **"
     )
 
 @bot.on(admin_cmd(pattern="ungban ?(.*)"))
@@ -132,11 +133,11 @@ async def gunban(userbot):
     sender = await ids.get_sender()
     hum = await ids.client.get_me()
     if not sender.id == hum.id:
-        PYTHONBOT = await edit_or_reply(ids, "`Trying to ungban this kid...`")
+        DARKMAMBA = await edit_or_reply(ids, "`UNGBAN KRNE KA TRY KAR RHA HU...`")
     else:
-        PYTHONBOT = await edit_or_reply(ids, "`Ungban in progress...`")
+        DARKMAMBA = await edit_or_reply(ids, "`UNGBAN HO RHA HAI SABAR RKH BETA...`")
     hum = await userbot.client.get_me()
-    await PYTHONBOT.edit(f"`Trying to ungban this kiddo...`")
+    await DARKMAMBA.edit(f"`UNGBAN KRNE KA TRY KAR RHA HU...`")
     my_mention = "[{}](tg://user?id={})".format(hum.first_name, hum.id)
     f"@{hum.username}" if hum.username else my_mention
     await userbot.get_chat()
@@ -154,10 +155,10 @@ async def gunban(userbot):
         if not reason:
             reason = "Private"
     except:
-        return await PYTHONBOT.edit("**Som3ting W3nt Wr0ng**")
+        return await DARKMAMBA.edit("**YE INSAAN PAIDA HI GALAT HUWA HAI TOO ISKI ID KAHA SE SAHI HOGI**")
     if user:
-        if user.id == 1938996006:
-            return await PYTHONBOT.edit("**You need to grow some balls to gban / ungban my creator**")
+        if user.id == DARKMAMBA:
+            return await DARKMAMBA.edit("**You need to grow some balls to gban / ungban my creator**")
         try:
             from userbot.modules.sql_helper.gmute_sql import ungmute
         except:
@@ -175,17 +176,17 @@ async def gunban(userbot):
             try:
                 await userbot.client.edit_permissions(i, user, send_messages=True)
                 a += 1
-                await PYTHONBOT.edit(f"Ok! Now Ungbaning ho rha h nhi to yhi patak kr chod dete.\nChats:- `{a}`")
+                await DARKMAMBA.edit(f"Ok! Now ABE OO PHLE TOO BAAP SE JEETA NHI JB GBAN DE DIYA TOO UNGBAN KRVAANE KE LIYE MERE LUND KO CHAAT RHA HAI.\nChats:- `{a}`")
             except:
                 b += 1
     else:
-        await PYTHONBOT.edit("**Reply to a user**")
+        await DARKMAMBA.edit("**Reply to a user**")
     try:
         if ungmute(user.id) is False:
-            return await PYTHONBOT.edit("**Error! User already ungbanned.**")
+            return await DARKMAMBA.edit("**Error! ISKO PHLE SE HI CHOD DIYA BINA CONDOM KE.**")
     except:
         pass
-    return await PYTHONBOT.edit(
+    return await DARKMAMBA.edit(
         f"**[{user.first_name}](tg://user?id={user.id}) Purani Baate bhul jaa... ab Friend ban ja 😂.**\n\nUngban Successful 🔥\nChats :- `{a}`"
     )
 
@@ -194,28 +195,28 @@ async def gunban(userbot):
 
 @borg.on(ChatAction)
 async def handler(legend): 
-   if python.user_joined or python.user_added:      
+   if DARKMAMBA.user_joined or DARKMAMBA.user_added:      
        try:       	
          from userbot.plugins.sql_helper.gmute_sql import is_gmuted
-         guser = await python.get_user()      
+         guser = await DARKMAMBA.get_user()      
          gmuted = is_gmuted(guser.id)             
        except:      
           return
        if gmuted:
         for i in gmuted:
             if i.sender == str(guser.id):                                                                         
-                chat = await python.get_chat()
+                chat = await DARKMAMBA.get_chat()
                 admin = chat.admin_rights
                 creator = chat.creator   
                 if admin or creator:
                  try:
                     await client.edit_permissions(python.chat_id, guser.id, view_messages=False)                              
-                    await python.reply(
+                    await DARKMAMBA.reply(
                      f"⚠️⚠️**Warning**⚠️⚠️\n\n`Gbanned User Joined the chat!!`\n"                      
                      f"**🔥 Victim Id 🔥**:\n[{guser.id}](tg://user?id={guser.id})\n"                   
                      f"**🔥 Action 🔥**  :\n`Banned this piece of shit....` **AGAIN!**")                                                
                  except:       
-                    python.reply("`Sheit!! No permission to ban users.\n@admins ban this retard.\nGlobally Banned User And A Potential Spammer`\n**Make your group a safe place by cleaning this shit**")                   
+                    DARKMAMBA.reply("`Sheit!! No permission to ban users.\n@admins ban this retard.\nGlobally Banned User And A Potential Spammer`\n**Make your group a safe place by cleaning this shit**")                   
                     return
                   
                   
