7a3a1be2c56abb50d75a91c9c13192e23892367d
HIDDENBOY <94282533+HIDDENB0Y@users.noreply.github.com>
2021-12-05 19:50:57 +0530

    Update _core.py

--- userbot/plugins/_core.py
+++ userbot/plugins/_core.py
@@ -5,13 +5,13 @@ from datetime import datetime
 from pathlib import Path
 from telethon import events, functions, types
 from telethon.tl.types import InputMessagesFilterDocument
-from PYTHONBOT.utils import *
+from DARKMAMBA.utils import *
 from userbot import *
 from . import *
 DELETE_TIMEOUT = 5
-DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else "『Pythonẞø†』"
-python = bot.uid
-PYTHON = f"[{DEFAULTUSER}](tg://user?id={python})"
+DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else "『🐼  🎀  𝒟𝒜𝑅𝒦𝑀𝒜𝑀𝐵𝒜  🎀  🐼』"
+darkmamba = bot.uid
+DARKMAMBA = f"[{DEFAULTUSER}](tg://user?id={darkmamba})"
 
 @bot.on(admin_cmd(pattern=r"send (?P<shortname>\w+)", outgoing=True))
 @bot.on(sudo_cmd(pattern=r"send (?P<shortname>\w+)", allow_sudo=True))
@@ -19,9 +19,9 @@ async def send(event):
     if event.fwd_from:
         return
     message_id = event.message.id
-    thumb = PYTHON_logo2
+    thumb = DARKMAMBA_logo2
     input_str = event.pattern_match.group(1)
-    omk = f"**⍟ 𝙿𝚕𝚞𝚐𝚒𝚗 𝚗𝚊𝚖𝚎 ≈** `{input_str}`\n**⍟ 𝚄𝚙𝚕𝚘𝚊𝚍𝚎𝚍 𝙱𝚢 ≈** {python_mention}\n\n⚡ **[Pythonẞø†](https://t.me/Python_Userbot_Support)** ⚡"
+    omk = f"**⍟ 𝙿𝚕𝚞𝚐𝚒𝚗 𝚗𝚊𝚖𝚎 ≈** `{input_str}`\n**⍟ 𝚄𝚙𝚕𝚘𝚊𝚍𝚎𝚍 𝙱𝚢 ≈** {darkmamba_mention}\n\n⚡ **[🐼  🎀  𝒟𝒜𝑅𝒦𝑀𝒜𝑀𝐵𝒜  🎀  🐼](https://t.me/DARKMAMBA_SUPPORT)** ⚡"
     the_plugin_file = "./userbot/plugins/{}.py".format(input_str)
     if os.path.exists(the_plugin_file):
         lauda = await event.client.send_file(
@@ -69,7 +69,7 @@ async def install(event):
                             a = "__𝙸𝚗𝚜𝚝𝚊𝚕𝚕𝚒𝚗𝚐...__"
                             b = 1
                         await event.edit(a)
-                    return await event.edit(f"✅ **𝙸𝚗𝚜𝚝𝚊𝚕𝚕𝚎𝚍 𝙼𝚘𝚍𝚞𝚕𝚎** :- `{shortname}` \n✨ BY :- {python_mention}\n\n{string}\n\n        ⚡ **[『Pythonẞø†』](t.me/Pythoon_Userbot_Support)**\n ⚠️Dont Try To Install External Plugin⚠️\n Click Here for Uninstall 👉`.uninstall {shortname}`⚡", link_preview=False)
+                    return await event.edit(f"✅ **𝙸𝚗𝚜𝚝𝚊𝚕𝚕𝚎𝚍 𝙼𝚘𝚍𝚞𝚕𝚎** :- `{shortname}` \n✨ BY :- {darkmamba_mention}\n\n{string}\n\n        ⚡ **[『🐼  🎀  𝒟𝒜𝑅𝒦𝑀𝒜𝑀𝐵𝒜  🎀  🐼』](t.me/DARKMAMBA_SUPPORT)**\n ⚠️Dont Try To Install External Plugin⚠️\n Click Here for Uninstall 👉`.uninstall {shortname}`⚡", link_preview=False)
                 return await event.edit(f"Installed module `{os.path.basename(downloaded_file_name)}`")
             else:
                 os.remove(downloaded_file_name)
@@ -138,7 +138,7 @@ async def install(event):
     if event.reply_to_msg_id:
         reply_to_id = event.reply_to_msg_id
     cmd = "ls userbot/plugins"
-    thumb = PYTHON_logo1
+    thumb = DARKMAMBA_logo1
     process = await asyncio.create_subprocess_shell(
         cmd, stdout=asyncio.subprocess.PIPE, stderr=asyncio.subprocess.PIPE
     )
@@ -146,11 +146,11 @@ async def install(event):
     o = stdout.decode()
     _o = o.split("\n")
     o = "\n".join(_o)
-    OUTPUT = f"♥️List Of Plugins In 𝖑𝖊ɠêɳ̃dẞø✞︎ 🇮🇳 :- \n\n{o}\n\n<><><><><><><><><><><><><><><><><><><><><><><><>\nHELP:- If you want to know the commands for a plugin, do :- \n.plinfo <plugin name> without the < > brackets. \nJoin https://t.me/Legend_Userbot for help."
+    OUTPUT = f"♥️List Of Plugins In MAMBA 🇮🇳 :- \n\n{o}\n\n<><><><><><><><><><><><><><><><><><><><><><><><>\nHELP:- If you want to know the commands for a plugin, do :- \n.plinfo <plugin name> without the < > brackets. \nJoin https://t.me/ for help."
     if len(OUTPUT) > 69:
         with io.BytesIO(str.encode(OUTPUT)) as out_file:
             out_file.name = "cmnds_list.text"
-            PYTHON_file = await bot.send_file(
+            DARKMAMBA_file = await bot.send_file(
                 event.chat_id,
                 out_file,
                 force_document=True,
@@ -158,7 +158,7 @@ async def install(event):
                 thumb=thumb,
                 reply_to=reply_to_id,
             )
-            await edit_or_reply(PYTHON_file, f"**Output Too Large. This is the file for the list of plugins in ✞︎t͛ẞ̸ Pythonẞø✞︎ .\n\n**BY :- {DEFAULTUSER}**")
+            await edit_or_reply(PYTHON_file, f"**Output Too Large. This is the file for the list of plugins in 🐼  🎀  𝒟𝒜𝑅𝒦𝑀𝒜𝑀𝐵𝒜  🎀  🐼 .\n\n**BY :- {DEFAULTUSER}**")
             await event.delete()
 
 
@@ -174,7 +174,7 @@ CmdHelp("core").add_command(
 ).add_command(
   "send", "<file name>", "Sends the given file from your userbot server, if any.", "send alive"
 ).add_command(
-  "cmds", None, "Gives out the list of modules in PYTHONBOT."
+  "cmds", None, "Gives out the list of modules in DARKMAMBA."
 ).add_type(
   "Official"
 ).add()
